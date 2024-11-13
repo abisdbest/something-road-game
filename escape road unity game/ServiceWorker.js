0c8b9bd9a6a5d77f23c60a783a751f77856fb021
abisdbest <105310709+abisdbest@users.noreply.github.com>
2024-11-13 08:17:29 +0000

    hi

---
+++ escape road unity game/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "DefaultCompany-my escape game-0.1";
+const contentToCache = [
+    "Build/escape road unity game.loader.js",
+    "Build/escape road unity game.framework.js.gz",
+    "Build/escape road unity game.data.gz",
+    "Build/escape road unity game.wasm.gz",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
